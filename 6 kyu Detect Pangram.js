832ecf04a86384eb574303bfba5d91bf1b0775c8
Kevin Rebakure <kevinrebakure@gmail.com>
2024-06-11 08:45:52 +0200

    detect pangram refactored with the use of Set

--- 6 kyu Detect Pangram.js
+++ 6 kyu Detect Pangram.js
@@ -5,30 +5,7 @@
 function isPangram(string) {
   // generate an array of all characters
   const text = string.toLowerCase().match(/[a-z]/g);
-  const alphabets = [];
-  for (let index = 97; index <= 122; index++) {
-    alphabets.push(String.fromCharCode(index));
-  }
-  // frequency of occurances
-  const data = Object.fromEntries([
-    ...alphabets.map((char) => [char.toLowerCase(), 0]),
-  ]);
-  alphabets.forEach((char) => {
-    let count = 0;
-    for (let s of text) {
-      if (s == char) {
-        count++;
-      }
-    }
-    data[char] = count;
-  });
-  //   final test
-  if ([...Object.values(data)].includes(0)) {
-    return false;
-  } else {
-    return true;
-  }
-  // return text
+  return [...new Set(text)].length == 26 ? true : false
 }
 
-console.log(isPangram("Pack my box with five dozen liquor jugs."));
+console.log(isPangram("kevin"));

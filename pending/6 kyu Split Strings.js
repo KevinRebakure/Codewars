f52a072f812358e733d7f429cfb8ace63cbbf330
Kevin Rebakure <kevinrebakure@gmail.com>
2024-06-13 10:57:59 +0200

    started 6 kyu split strings

---
+++ pending/6 kyu Split Strings.js
@@ -0,0 +1,15 @@
+// Question: https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/javascript
+
+// 'abc' =>  ['ab', 'c_']
+// 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str) {
+  let n = Math.floor(str.length / 2);
+  let text = str.split('')
+  let arr = []
+  for(let i = 0; i < n; i++) {
+    
+  }
+}
+
+console.log(solution("abcdef"));

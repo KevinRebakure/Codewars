21edc67f78922e06b78d64697df141993ca650e2
Kevin Rebakure <kevinrebakure@gmail.com>
2024-06-16 21:41:35 +0200

    8 kyu short long short done✅

---
+++ 8 kyu/Short Long Short.js
@@ -0,0 +1,12 @@
+// Question: https://www.codewars.com/kata/50654ddff44f800200000007/train/javascript
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+
+function solution(a, b) {
+  return `${a.length < b.length ? a : b}${a.length > b.length ? a : b}${
+    a.length < b.length ? a : b
+  }`;
+}
+
+// console.log(solution("1", "22"));

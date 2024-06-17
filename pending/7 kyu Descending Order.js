8f0d4e49a270caf7857864d4e3507bac6d317058
Kevin Rebakure <kevinrebakure@gmail.com>
2024-06-17 10:46:23 +0200

    working on ⌛- 7 kyu Descending Order

---
+++ pending/7 kyu Descending Order.js
@@ -0,0 +1,11 @@
+// Question: https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
+
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+  //...
+}

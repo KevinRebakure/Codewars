332ba48a68f4b9a716da2ed5f06dd7c65634d3bd
Kevin Rebakure <kevinrebakure@gmail.com>
2024-06-17 10:33:31 +0200

    working on ⌛- 6 kyu Give me a Diamond

---
+++ pending/6 kyu Give me a Diamond.js
@@ -0,0 +1,21 @@
+// Question: https://www.codewars.com/kata/5503013e34137eeeaa001648/train/javascript
+
+//   *
+//  ***
+// *****
+//  ***
+//   *
+
+// You need to return a string that looks like a diamond 
+// shape when printed on the screen, using asterisk (*) characters. 
+// Trailing spaces should be removed, and every line must be terminated
+//  with a newline character (\n).
+
+// "  *\n ***\n*****\n ***\n  *\n";
+
+// Return null/nil/None/... if the input is an even number or
+//  negative, as it is not possible to print a diamond of even or negative size.
+
+function diamond(n) {
+  return "";
+}

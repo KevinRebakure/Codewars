58f5202e23708b24fa8ffd0f6d9b5a9f73901b2f
Rebakure <kevinrebakure@gmail.com>
2024-06-18 11:15:29 +0200

    working on ⌛ - 6 kyu Give me a diamond

---
+++ pending/6 kyu Temperature converter.js
@@ -0,0 +1,16 @@
+// Question: https://www.codewars.com/kata/54ce9497975ca65e1a0008c6/train/javascript
+
+
+// possible scale inputs:
+//     "C"  for Celsius
+//     "F"  for Fahrenheit
+//     "K"  for Kelvin
+//     "R"  for Rankine
+//     "De" for Delisle
+//     "N"  for Newton
+//     "Re" for Réaumur
+//     "Ro" for Rømer
+
+function convertTemp(temp, from_scale, to_scale) {
+  // ...
+}

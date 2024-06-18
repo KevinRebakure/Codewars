3d749bfbff7f16b2bdc3bbb3bd695f8ae2ee7920
Rebakure <kevinrebakure@gmail.com>
2024-06-18 10:42:09 +0200

    done ✅ - 7 kyu highest and lowest

--- 7 kyu/Highest and Lowest.js
+++ 7 kyu/Highest and Lowest.js
@@ -4,5 +4,8 @@
 // highAndLow("1 2 -3 4 5"); // return "5 -3"
 // highAndLow("1 9 3 4 -5"); // return "9 -5"
 function highAndLow(numbers) {
-  // ...
+  const arr = numbers.split(' ').map((num)=>parseInt(num))
+  return `${Math.max(...arr)} ${Math.min(...arr)}`
 }
+
+// console.log(highAndLow('1 2 3 4 5'))

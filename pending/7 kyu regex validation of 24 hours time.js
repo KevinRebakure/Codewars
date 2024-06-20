5094ed179853d7ff7f46fd16706e44220c1880f6
Rebakure <kevinrebakure@gmail.com>
2024-06-20 11:14:41 +0200

    working on ⌛ - 7 kyu regex validation of 24 hours time

---
+++ pending/7 kyu regex validation of 24 hours time.js
@@ -0,0 +1,7 @@
+// Question: https://www.codewars.com/kata/56a4a3d4043c316002000042/train/javascript
+
+function validateTime(time) {
+  return /(([0-2][0-9])|([0-9])):[0-5][0-9]/gi.test(time);
+}
+
+console.log(validateTime('01:12'))

585b58099b0ebe1ec0afdbcfd845dee8e3075a7c
Rebakure <kevinrebakure@gmail.com>
2024-06-24 18:10:46 +0200

    Update 5 kyu RGB To Hex Conversion.js

--- pending/5 kyu RGB To Hex Conversion.js
+++ pending/5 kyu RGB To Hex Conversion.js
@@ -1,5 +1,7 @@
 // Question: https://www.codewars.com/kata/513e08acc600c94f01000001/train/javascript
 
 function rgb(r, g, b) {
-  return "FFFFFF";
+  return `${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
 }
+
+console.log(rgb(0,0,0))

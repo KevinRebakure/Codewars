c1a8f62edce7a82560dc4b1603237ee1de66185f
Rebakure <kevinrebakure@gmail.com>
2024-06-25 11:44:23 +0200

    Create Exercise 9.js

---
+++ Asynchronous Exercises/Exercise 9.js
@@ -0,0 +1,8 @@
+const xhr = new XMLHttpRequest()
+xhr.open("GET", "https://jsonplaceholder.typicode.com/comments");
+xhr.onreadystatechange = function() {
+    if (condition) {
+        
+    }
+}
+xhr.send()

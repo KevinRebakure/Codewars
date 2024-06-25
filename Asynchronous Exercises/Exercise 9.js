acc2c1db6575761beb5e5d496390ee5fa9bdced7
Rebakure <kevinrebakure@gmail.com>
2024-06-25 15:15:51 +0200

    done ✅ - Exercise 9

--- Asynchronous Exercises/Exercise 9.js
+++ Asynchronous Exercises/Exercise 9.js
@@ -1,8 +1,15 @@
-const xhr = new XMLHttpRequest()
-xhr.open("GET", "https://jsonplaceholder.typicode.com/comments");
-xhr.onreadystatechange = function() {
-    if (condition) {
-        
-    }
+function xhrRequest() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const response = this.responseText
+            console.log(response)
+        } else if (this.readyState == 4 && this.status !=200) {
+            console.log('Error happened!! 😭😭')
+        }
+    };
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/comments");
+    xhr.send();
 }
-xhr.send()
+
+xhrRequest()

f4148dc102340beb60e040d45d370541ae4f2827
Rebakure <kevinrebakure@gmail.com>
2024-06-27 08:04:07 +0200

    done ✅ - 7 kyu regex validation for 24 hours time

--- 7 kyu/regex validation of 24 hours time.js
+++ 7 kyu/regex validation of 24 hours time.js
@@ -1,11 +1,8 @@
 // Question: https://www.codewars.com/kata/56a4a3d4043c316002000042/train/javascript
 
 function validateTime(time) {
-  if (time != "") {
-    return time == /(([0-2][0-4])|([0-1][0-9])|([0-9])):[0-5][0-9]/gi
-      ? true
-      : false;
-  }
+  const timeRegex = /^(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+  return timeRegex.test(time);
 }
 
 console.log(validateTime("24:12"));

edee74be0d40e3b244914161715356bafba747e4
Rebakure <kevinrebakure@gmail.com>
2024-06-27 16:28:53 +0200

    Update Exercise 2.js

--- Asynchronous Exercises/Previous Gate/Exercise 2.js
+++ Asynchronous Exercises/Previous Gate/Exercise 2.js
@@ -1,17 +1,17 @@
 async function fetchData() {
-  const response = await fetch(
-    "https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json&page=2"
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      "https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json&page=2"
+    ).then((res) => res.json());
+
+    const data = response.Results.filter(
+      (manufacturer) => manufacturer.Country == "UNITED STATES (USA)"
+    );
+
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-fetchData().then((message) => {
-  console.log(
-    message.Results.filter((manufacturer) => {
-      if (manufacturer.Country == "UNITED STATES (USA)") {
-        return manufacturer;
-      }
-    })
-  );
-});
+fetchData();

29afa6c87d528e4811d23b86c22192cccfaa2e3c
Rebakure <kevinrebakure@gmail.com>
2024-06-28 08:07:49 +0200

    Update 6 kyu Even or Odd Accessor.js

--- pending/6 kyu Even or Odd Accessor.js
+++ pending/6 kyu Even or Odd Accessor.js
@@ -1,5 +1,33 @@
 // Question: https://www.codewars.com/kata/6656a4687f3e4eb5fb520817/train/javascript
 
+// evenOrOdd(2); //'Even'
+// evenOrOdd[2]; //'Even'
+// evenOrOdd(7); //'Odd'
+// evenOrOdd[7]; //'Odd'
+
 function evenOrOdd(n) {
-  //'Even' or 'Odd'
+  if (typeof n === "number") {
+    switch (n % 2) {
+      case 0:
+        return "Even";
+      default:
+        return "Odd";
+    }
+  } else if (typeof n == "object") {
+    switch (n[0] % 2) {
+      case 0:
+        return "Even";
+      default:
+        return "Odd";
+    }
+  } else if (typeof n == "string") {
+    switch (parseInt(n) % 2) {
+      case 0:
+        return "Even";
+      default:
+        return "Odd";
+    }
+  }
 }
+
+console.log(evenOrOdd(0));

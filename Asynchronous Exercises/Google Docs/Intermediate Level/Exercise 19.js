50c9490abec57c641da1dbc4ec8a64ba55392e82
Rebakure <kevinrebakure@gmail.com>
2024-06-30 19:36:45 +0200

    Update Exercise 19.js

--- Asynchronous Exercises/Google Docs/Intermediate Level/Exercise 19.js
+++ Asynchronous Exercises/Google Docs/Intermediate Level/Exercise 19.js
@@ -1,2 +1,25 @@
 // Question: 9. Implement a simple timeout wrapper for a Promise that rejects if
 // the Promise does not resolve within a specified time
+
+async function race() {
+  try {
+    const promise = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(fetch("https://jsonplaceholder.typicode.com/users"));
+        }, 0);
+
+        setTimeout(() => {
+          reject("Reject❌");
+        }, 0);
+      });
+    };
+
+    const data = await promise();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+race();

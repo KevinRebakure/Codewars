2988e6d31165b3679e04c5754be794ea76e287b9
Rebakure <kevinrebakure@gmail.com>
2024-07-03 16:59:48 +0200

    working on ⌛- 7 kyu build a square

---
+++ TypeScript/dist/pending/7 kyu Build a square.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateShape = generateShape;
+function generateShape(int) {
+    return "+";
+}
---
+++ TypeScript/src/pending/7 kyu Build a square.ts
@@ -0,0 +1,5 @@
+// Question: https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/train/typescript
+
+export function generateShape(int: number): string {
+  return "+"; // your code here
+}

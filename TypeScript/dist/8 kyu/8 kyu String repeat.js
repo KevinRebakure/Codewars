13f8e84cebdf1bd9bac14fb636f860a0cec0c43e
Rebakure <kevinrebakure@gmail.com>
2024-07-03 16:02:37 +0200

    8 kyu string repeat

---
+++ TypeScript/dist/8 kyu/8 kyu String repeat.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.repeatStr = repeatStr;
+function repeatStr(n, s) {
+    return s.repeat(n);
+}
---
+++ TypeScript/src/8 kyu/8 kyu String repeat.ts
@@ -0,0 +1,5 @@
+// Question: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/typescript
+
+export function repeatStr(n: number, s: string): string {
+    return s.repeat(n)
+}

44a73180c02519a7d6d9efda9284a48d91a2bb0b
Rebakure <kevinrebakure@gmail.com>
2024-07-03 16:08:53 +0200

    8 kyu remove first and last character

---
+++ TypeScript/dist/8 kyu/8 kyu Remove First and Last Character.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.removeChar = removeChar;
+function removeChar(str) {
+    return str.slice(1, str.length - 1);
+}

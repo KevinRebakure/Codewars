660ec8f507c1cd167032cb7b0a24e3edc518343d
Rebakure <kevinrebakure@gmail.com>
2024-07-06 09:55:46 +0200

    directory changes

--- TypeScript/src/pending/7 kyu How many are smaller than me.ts
+++ TypeScript/src/pending/7 kyu How many are smaller than me.ts
@@ -1,15 +1,8 @@
 // Question: https://www.codewars.com/kata/56a1c074f87bc2201200002e/train/typescript
 
-export function smaller(nums: number[]): number[] {
-  return nums.map((num: number, i: number, arr: number[]): number => {
-    let count: number = 0;
-    for (let j: number = 1; j <= arr.slice(i).length; j++) {
-      if (arr[j] < num) {
-        count++;
-      }
-    }
-    return count;
-  });
-}
+// * Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
+// * Input [1, 2, 0] => Output [1, 1, 0]
 
-console.log(smaller([5, 4, 3, 2, 1]));
+export function smaller(nums: number[]): number[] {
+  const 
+}

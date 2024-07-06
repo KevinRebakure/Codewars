c222a74f8fb2625b1c21787a97e5e57b1dbc2f12
Rebakure <kevinrebakure@gmail.com>
2024-07-06 10:09:29 +0200

    done ✅ - 7 kyu how many are smaller than me

--- TypeScript/dist/pending/7 kyu How many are smaller than me.js
+++ TypeScript/dist/pending/7 kyu How many are smaller than me.js
@@ -2,5 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.smaller = smaller;
 function smaller(nums) {
-    const ;
+    const small = nums.map((num, i) => {
+        let count = 0;
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < num) {
+                count++;
+            }
+        }
+        return count;
+    });
+    return small;
 }
+console.log(smaller([5, 4, 3, 2, 1]));
--- TypeScript/src/pending/7 kyu How many are smaller than me.ts
+++ TypeScript/src/pending/7 kyu How many are smaller than me.ts
@@ -4,5 +4,29 @@
 // * Input [1, 2, 0] => Output [1, 1, 0]
 
 export function smaller(nums: number[]): number[] {
-  const 
+  const small: number[] = nums.map((num:number, i:number):number=>{
+    let count = 0;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < num) {
+        count++;
+      }
+    }
+    return count
+  });
+  return small;
 }
+// export function smaller(nums: number[]): number[] {
+//   const small: number[] = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     let count = 0;
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] < nums[i]) {
+//         count++;
+//       }
+//     }
+//     small.push(count);
+//   }
+//   return small;
+// }
+
+console.log(smaller([5, 4, 3, 2, 1]));

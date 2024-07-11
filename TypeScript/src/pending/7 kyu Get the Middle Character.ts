dfef3cf74f507840c4e01fbfcb1a6635401f9e24
Rebakure <kevinrebakure@gmail.com>
2024-07-11 16:46:43 +0200

    pending⏳- middle character

---
+++ TypeScript/src/pending/7 kyu Get the Middle Character.ts
@@ -0,0 +1,20 @@
+// Question: https://www.codewars.com/kata/56747fd5cb988479af000028/train/typescript
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+
+export function getMiddle(s: string) {
+    if (s.length % 2 !== 0) {
+        return s.charAt(Math.trunc(s.length / 2))
+    } else {
+        return ''
+    }
+    // return the middle charater(s)
+}
+
+console.log(getMiddle('kevin'))

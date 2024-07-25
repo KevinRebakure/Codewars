21def1810a3e15650b1ca9e79545a2c58a3f7775
Rebakure <kevinrebakure@gmail.com>
2024-07-25 16:42:04 +0200

    mobile too long

--- TypeScript/pending/6 kyu Texting with an old-school mobile phone.ts
+++ TypeScript/pending/6 kyu Texting with an old-school mobile phone.ts
@@ -1,26 +1,48 @@
 // https://www.codewars.com/kata/5ca24526b534ce0018a137b5/train/typescript
 
+// 'hey', '4433999'
+
 export function sendMessage(message: string): string {
-    const keysArr = new Map<number | string, string>([
-        [1, '.,?!'],
-        [2, 'abc'],
-        [3, 'def'],
-        [4, 'ghi'],
-        [5, 'jkl'],
-        [6, 'mno'],
-        [7, 'pqrs'],
-        [8, 'tuv'],
-        [9, 'wxyz'],
+    const keysArr = new Map([
+        ['1', '.,?!'],
+        ['2', 'abc'],
+        ['3', 'def'],
+        ['4', 'ghi'],
+        ['5', 'jkl'],
+        ['6', 'mno'],
+        ['7', 'pqrs'],
+        ['8', 'tuv'],
+        ['9', 'wxyz'],
         ['*', '-+='],
-        [0, 'space'],
-        ['#', 'case'],
+        ['0', ''],
+        ['#', ''],
     ])
 
-    message.split('').forEach((char)=>{
-        console.log(char)
+    // const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+    // const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    // let lower = true
+
+    message.split('').forEach((char, i) => {
+        if (char === ' ') {
+            console.log('space')
+        } else {
+            for (const [key, value] of keysArr.entries()) {
+                if (char !== key) {
+                    if (value.includes(char)) {
+                        const index = value.indexOf(char) + 1
+                        console.log(key.toString().repeat(index))
+                    }
+                } else if (char === key) {
+                    console.log(`${char}-`)
+                } else if (i !== 0 && char === message[i + 1]) {
+                    console.log('wait')
+                }
+            }
+        }
     })
 
     return message
 }
 
-sendMessage('kevin')
+sendMessage('555')

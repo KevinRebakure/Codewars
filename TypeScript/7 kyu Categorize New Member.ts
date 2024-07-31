5d696747af8db0949a5b28aa9d7069176475c647
Rebakure <kevinrebakure@gmail.com>
2024-07-31 14:27:31 +0200

    Create 7 kyu Categorize New Member.ts

---
+++ TypeScript/7 kyu Categorize New Member.ts
@@ -0,0 +1,7 @@
+// Question: https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/typescript
+
+export function openOrSenior(data: number[][]): string[] {
+    return data.map((data) =>
+        data[0] >= 55 && data[1] > 7 ? 'Senior' : 'Open'
+    )
+}

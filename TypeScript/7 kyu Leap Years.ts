f3d3e92098bd805e1791ccb8e6f90d1dd668c2d8
Rebakure <kevinrebakure@gmail.com>
2024-07-31 14:15:26 +0200

    Create 7 kyu Leap Years.ts

---
+++ TypeScript/7 kyu Leap Years.ts
@@ -0,0 +1,10 @@
+// Question: https://www.codewars.com/kata/526c7363236867513f0005ca/train/typescript
+
+export function isLeap(year: number): boolean {
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false
+    } else if (year % 4 === 0 || year % 400 === 0) {
+        return true
+    }
+    return false
+}

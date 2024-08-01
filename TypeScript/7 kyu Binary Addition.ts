f7eae775a223319bb5d221a04c73fe7683a616d1
Rebakure <kevinrebakure@gmail.com>
2024-08-01 13:44:11 +0200

    Create 7 kyu Binary Addition.ts

---
+++ TypeScript/7 kyu Binary Addition.ts
@@ -0,0 +1,7 @@
+// Question: https://www.codewars.com/kata/551f37452ff852b7bd000139/train/typescript
+
+export function addBinary(a: number, b: number): string {
+    return (a + b).toString(2)
+}
+
+console.log(addBinary(1, 1))

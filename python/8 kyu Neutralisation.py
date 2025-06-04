3339fd58a5861cd55f4e2247c1ff73c36eca1f64
Rebakure <kevinrebakure@gmail.com>
2025-06-04 10:02:37 +0200

    8 kyu Neutralisation

---
+++ python/8 kyu Neutralisation.py
@@ -0,0 +1,37 @@
+# https://www.codewars.com/kata/65128732b5aff40032a3d8f0/train/python
+def neutralise(s1, s2):
+    if(len(s1) != len(s2)): return None 
+    neutral=''
+    for index in range(len(s1)):
+      if s1[index] == '+' and s2[index] == '+':
+          neutral+= '+'
+      if s1[index] == '-' and s2[index] == '-':
+          neutral+= '-'
+      if s1[index] == '+' and s2[index] == '-':
+          neutral+= '0'
+      if s1[index] == '-' and s2[index] == '+':
+          neutral+= '0'
+    return neutral
+
+print(neutralise("--++--", "++--++"))
+
+#  test.assert_equals(neutralise("--++--", "++--++"), "000000")
+#         test.assert_equals(neutralise("-+-+-+", "-+-+-+"), "-+-+-+")
+#         test.assert_equals(neutralise("-++-", "-+-+"), "-+00")
+#         test.assert_equals(neutralise("--++", "++++"), "00++")
+#         test.assert_equals(neutralise("+++--+---", "++----++-"), "++0--000-")
+#         test.assert_equals(neutralise("-----", "-----"), "-----")
+#         test.assert_equals(neutralise("-+", "++"), "0+")
+#         test.assert_equals(neutralise("--", "-+"), "-0")
+#         test.assert_equals(neutralise("-++", "+--"), "000")
+#         test.assert_equals(neutralise("++-++--++-", "-+++-++-++"), "0+0+0000+0")
+#         test.assert_equals(neutralise("-++-+-++-", "+-++++---"), "00+0+000-")
+#         test.assert_equals(neutralise("---++-+--", "-+++--++-"), "-00+0-+0-")
+#         test.assert_equals(neutralise("+-----+++-", "--+-+-++--"), "0-0-0-++0-")
+#         test.assert_equals(neutralise("+-----+-", "---++-++"), "0--00-+0")
+#         test.assert_equals(neutralise("-+--+-+---", "-+--+-+-+-"), "-+--+-+-0-")
+#         test.assert_equals(neutralise("+-+", "-++"), "00+")
+#         test.assert_equals(neutralise("-++", "-+-"), "-+0")
+#         test.assert_equals(neutralise("---+", "-+++"), "-00+")
+#         test.assert_equals(neutralise("+--", "+--"), "+--")
+#         test.assert_equals(neutralise("--+++-+-", "+++++---"), "00+++-0-")

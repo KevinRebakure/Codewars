1deb8eba68b4c2cf394e9a7c5dc832fc615f57d5
Rebakure <kevinrebakure@gmail.com>
2025-06-09 09:44:55 +0200

    8 kyu Generate user method 2 usign F-string

--- python/8 kyu Generate user links.py
+++ python/8 kyu Generate user links.py
@@ -2,3 +2,7 @@ import urllib.parse
 def generate_link(user):
     url="http://www.codewars.com/users/"
     return url + urllib.parse.quote(user)
+
+
+def method02(user):
+    return f"http://www.codewars.com/users/{urllib.parse.quote(user)}"

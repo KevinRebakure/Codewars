50d23d3c6c4887db337669f5bf0041ca5f2948b8
Rebakure <kevinrebakure@gmail.com>
2025-06-18 07:40:01 +0200

    8 kyu Plural

---
+++ python/8 kyu Plural.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/python
+def plural(n):
+    if n == 0:
+        return True
+    if n == 1:
+        return False
+    if n > 1:
+        return True
+    
+    

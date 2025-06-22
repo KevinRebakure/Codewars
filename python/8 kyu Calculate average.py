3923ea55e05746637b655060d34bbe193d184087
Rebakure <kevinrebakure@gmail.com>
2025-06-22 20:13:29 +0200

    8 kyu Calculate average

---
+++ python/8 kyu Calculate average.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/python
+
+def find_average(numbers):
+    if(len(numbers)==0): return 0
+    sum=0
+    for number in numbers:
+        sum+=number
+    return sum/len(numbers)

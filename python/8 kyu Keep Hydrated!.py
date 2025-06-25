c0d1a5ded2f417c09d67e919ea3cc2f0cb632162
Rebakure <kevinrebakure@gmail.com>
2025-06-25 06:39:57 +0200

    8 kyu keep hydrated

---
+++ python/8 kyu Keep Hydrated!.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/582cb0224e56e068d800003c/train/python
+from math import floor
+
+def litres(time):
+    return floor(time * 0.5)
+
+
+def floor_division(time):
+    return time // 2

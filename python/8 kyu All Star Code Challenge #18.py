3221bfc17e4563c022824492d9177386632943e0
Rebakure <kevinrebakure@gmail.com>
2025-07-07 04:08:55 +0200

    8 kyu All Star Code Challenge #18.py

---
+++ python/8 kyu All Star Code Challenge #18.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5865918c6b569962950002a1
+
+def str_count(strng, letter):
+    count=0
+    for character in strng:
+        if character == letter:
+            count += 1
+    return count

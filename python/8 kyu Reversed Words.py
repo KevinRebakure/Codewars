ef28c0b64674fe24bd27968897dcf54e9ae83be7
Rebakure <kevinrebakure@gmail.com>
2025-07-08 06:03:28 +0200

    8 kyu Reversed Words.py

--- python/8 kyu Reversed Words.py
+++ python/8 kyu Reversed Words.py
@@ -5,3 +5,7 @@ def reverse_words(s):
     string_list.reverse()
     return ' '.join(string_list)
 print(reverse_words('The greatest victory is that which requires no battle'))
+
+
+def with_new_reverse_method(string):
+    return ' '.join(string.split()[::-1])

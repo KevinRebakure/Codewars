bdd678eb90d90eca648a1a78480b89da5d5b0f8a
Rebakure <kevinrebakure@gmail.com>
2025-07-13 10:19:01 +0200

    7 kyu Jaden Casing Strings.py

---
+++ python/7 kyu Jaden Casing Strings.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/python
+
+
+def to_jaden_case(string):
+    string_list = string.split()
+    final_list = []
+    for word in string_list:
+        final_list.append(word.capitalize())
+    return ' '.join(final_list)
+
+print(to_jaden_case("How can mirrors be real if our eyes aren't real"))

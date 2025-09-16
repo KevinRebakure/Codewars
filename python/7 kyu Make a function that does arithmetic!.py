5aa45fab08fd5de1c203c70eba10bc13b65e4cf9
Rebakure <kevinrebakure@gmail.com>
2025-09-16 09:17:58 +0200

    7 kyu Make a function that does arithmetic!.py

---
+++ python/7 kyu Make a function that does arithmetic!.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/python
+
+def arithmetic(a, b, operator):
+    match operator.lower(): 
+        case 'add':
+            return a + b
+        case 'subtract':
+            return a - b
+        case 'multiply':
+            return a * b
+        case 'divide':
+            return a / b
+        case _:
+            return 'Invalid operation!'

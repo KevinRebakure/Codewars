678c923cd7c48d9a314e6525017baef7cc326a24
Rebakure <kevinrebakure@gmail.com>
2025-10-05 13:53:42 +0200

    8 kyu Even or Odd

---
+++ JavaWars/src/main/java/org/example/EvenOrOdd8kyu.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class EvenOrOdd8kyu {
+    static void main(String[] args) {
+        System.out.println( evenOrOdd(4));
+
+    }
+
+    public static String evenOrOdd(int number) {
+        return number % 2 == 0 ? "Even" : "Odd"; // Place code here;
+    }
+}

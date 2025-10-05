ab9ca392559f334f55abde825ccb840e90e547f0
Rebakure <kevinrebakure@gmail.com>
2025-10-05 14:04:43 +0200

    8 kyu Convert a Number to a String!

---
+++ JavaWars/src/main/java/org/example/ConvertNumberToString8kyu.java
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/java
+
+package org.example;
+
+public class ConvertNumberToString8kyu {
+    static void main(String[] args) {
+        System.out.println(numberToString(12));
+    }
+
+    public static String numberToString(int num) {
+        return String.valueOf(num); // Return a string of the number here!
+    }
+}

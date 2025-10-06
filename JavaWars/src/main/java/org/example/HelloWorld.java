9df8a9e9b4d8f47ad30cc551fade65613f2987f2
Rebakure <kevinrebakure@gmail.com>
2025-10-06 08:53:13 +0200

    8 kyu Function 1 - hello world

---
+++ JavaWars/src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/523b4ff7adca849afe000035/train/java
+package org.example;
+
+public class HelloWorld {
+    static void main(String[] args) {
+        System.out.println(greet());
+    }
+
+    public static String greet(){
+        return "hello world!";
+    }
+}

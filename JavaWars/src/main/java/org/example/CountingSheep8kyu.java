0f9b22353e71030642153944abf0d94be4a27b28
Rebakure <kevinrebakure@gmail.com>
2025-10-06 10:04:29 +0200

    8 kyu Counting sheep...

---
+++ JavaWars/src/main/java/org/example/CountingSheep8kyu.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class CountingSheep8kyu {
+    static void main(String[] args) {
+        System.out.println(countSheeps(new Boolean[] {true,  true,  true,  false,
+                true,  true,  true,  true ,
+                true,  false, true,  false,
+                true,  false, false, true ,
+                true,  true,  true,  true ,
+                false, false, true,  true}));
+    }
+
+    public static int countSheeps(Boolean[] arrayOfSheep) {
+        int count= 0;
+
+        for (Boolean sheep : arrayOfSheep) {
+            if (sheep != null && sheep) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

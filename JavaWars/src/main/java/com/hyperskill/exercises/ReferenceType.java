cbec1eacbcea893199b5161e2a4b71cf5b2396e1
Rebakure <kevinrebakure@gmail.com>
2025-10-07 14:45:43 +0200

    reference type

---
+++ JavaWars/src/main/java/com/hyperskill/exercises/ReferenceType.java
@@ -0,0 +1,8 @@
+package com.hyperskill.exercises;
+
+public class ReferenceType {
+    public static void main(String[] args) {
+        char ch = 65;
+        System.out.println(ch);
+    }
+}
--- JavaWars/src/main/java/org/example/Main.java
+++ JavaWars/src/main/java/org/example/Main.java
@@ -2,8 +2,24 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        System.out.println("Kevin");
+class Main {
+    public static void main(String[] args) {
+        int greatestPrimeInRange = 2;
+        for (int number = 2; number <= 1000; number++) {
+            if (isPrime(number) && number > greatestPrimeInRange) {
+                greatestPrimeInRange = number;
+            }
+        }
+    }
+
+    static boolean isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i != 0) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }

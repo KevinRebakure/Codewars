526e3c663c4b7a63470884d161100b429ef2b453
Rebakure <kevinrebakure@gmail.com>
2025-10-07 20:40:12 +0200

    compression algorithm

---
+++ JavaWars/src/main/java/org/java/codewars/CompressionAlgorithm.java
@@ -0,0 +1,28 @@
+package org.java.codewars;
+
+import java.util.Scanner;
+
+public class CompressionAlgorithm {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String rawValue = scanner.next();
+
+        int count = 1;
+        String encodedText = "";
+
+        for (int i = 0; i < rawValue.length(); i++) {
+            // Check if next character exists and is the same
+            if (i < rawValue.length() - 1 && rawValue.charAt(i) == rawValue.charAt(i + 1)) {
+                count++;
+            } else {
+                encodedText = encodedText + rawValue.charAt(i) + count;
+                count = 1;
+            }
+        }
+
+        System.out.println(encodedText);
+
+        scanner.close();
+    }
+}

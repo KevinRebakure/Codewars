b8060e66e60b3b95e466f925c6993d1fa10851cf
Rebakure <kevinrebakure@gmail.com>
2025-10-10 11:56:05 +0200

    extract morgage calculation logic

--- JavaWars/src/main/java/com/ultimatejava/exercises/MortgageCalculator.java
+++ JavaWars/src/main/java/com/ultimatejava/exercises/MortgageCalculator.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class MortgageCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
 
         // Get the principal
         System.out.print("Principal ($1k - $1M): ");
@@ -27,26 +25,31 @@ public class MortgageCalculator {
 
         // Get period in years
         System.out.print("Period (Years): ");
-        int periodYears = scanner.nextInt();
+        byte periodYears = scanner.nextByte();
         while (!(periodYears >= 0 && periodYears <= 30)) {
             System.out.println("Enter a value between 1 and 30: ");
-            periodYears = scanner.nextInt();
+            periodYears = scanner.nextByte();
         }
 
-        // Convert annual rate to monthly and period to months
-
-        double monthlyInterest = (annualInterestRate / PERCENT) / MONTHS_IN_YEAR;
-        int numberOfPayments = periodYears * MONTHS_IN_YEAR;
-
-        // Calculate mortgage using the correct formula
-        double mortgage = principal
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
         // Display formatted mortgage
+        double mortgage = calculateMortgage(principal, annualInterestRate, periodYears);
         String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + formattedMortgage);
 
         scanner.close();
     }
+
+    public static double calculateMortgage(int principal, double annualInterestRate, byte periodYears) {
+        final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12;
+
+        double monthlyInterest = (annualInterestRate / PERCENT) / MONTHS_IN_YEAR;
+        int numberOfPayments = periodYears * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    };
 }

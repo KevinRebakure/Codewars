dfbdb39c6d93d7105fbf8bc126e0872c9fcce78a
Rebakure <kevinrebakure@gmail.com>
2025-10-13 11:31:01 +0200

    opposites attract

---
+++ JavaWars/src/main/java/org/java/codewars/OppositesAttract8kyu.java
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/java
+package org.java.codewars;
+
+public class OppositesAttract8kyu {
+    public static boolean isLove(final int flower1, final int flower2) {
+        return (flower1 + flower2) % 2 != 0;
+    };
+}

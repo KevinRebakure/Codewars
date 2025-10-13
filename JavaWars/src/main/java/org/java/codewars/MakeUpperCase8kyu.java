f4e986e59c7418a6f230452876d957ed951f2daf
Rebakure <kevinrebakure@gmail.com>
2025-10-13 11:44:30 +0200

    make uppercase 8 kyu

---
+++ JavaWars/src/main/java/org/java/codewars/MakeUpperCase8kyu.java
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/java
+package org.java.codewars;
+
+public class MakeUpperCase8kyu {
+    public static String makeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}

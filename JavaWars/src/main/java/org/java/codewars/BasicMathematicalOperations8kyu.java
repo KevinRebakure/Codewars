492efdebc6bf301ad37ddeaaebf54216411c3c9c
Rebakure <kevinrebakure@gmail.com>
2025-10-13 11:22:59 +0200

    basic mathematical operations

---
+++ JavaWars/src/main/java/org/java/codewars/BasicMathematicalOperations8kyu.java
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/java
+package org.java.codewars;
+
+public class BasicMathematicalOperations8kyu {
+    static void main(String[] args) {
+
+    }
+    public static Integer basicMath(String op, int v1, int v2) {
+        return switch (op) {
+            case "+" -> v1 + v2;
+            case "-" -> v1 - v2;
+            case "*" -> v1 * v2;
+            case "/" -> v1 / v2;
+            default -> 0;
+        };
+    };
+}

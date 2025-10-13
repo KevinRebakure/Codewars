678335baff9d99b928be5ef29c276d74e1270c21
Rebakure <kevinrebakure@gmail.com>
2025-10-13 11:54:09 +0200

    beginner series clock 8 kyu

---
+++ JavaWars/src/main/java/org/java/codewars/Clock8kyu.java
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/java
+package org.java.codewars;
+
+public class Clock8kyu {
+    static void main(String[] args) {
+
+    }
+    public static int Past(int h, int m, int s) {
+        int hToMilliseconds = h * 60 * 60 * 1000;
+        int mToMilliseconds = m * 60 * 1000;
+        int sToMilliseconds = s * 1000;
+
+        return hToMilliseconds + mToMilliseconds + sToMilliseconds;
+    };
+}

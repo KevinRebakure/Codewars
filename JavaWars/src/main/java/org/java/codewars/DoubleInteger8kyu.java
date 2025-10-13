796f8ef2fb6650fd24de456f61818ecd1d2f7011
Rebakure <kevinrebakure@gmail.com>
2025-10-13 11:15:30 +0200

    8 kyu you can't code under pressure

---
+++ JavaWars/src/main/java/org/java/codewars/DoubleInteger8kyu.java
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/53ee5429ba190077850011d4/train/java
+package org.java.codewars;
+
+public class DoubleInteger8kyu {
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i * 2;
+    }
+}
--- JavaWars/src/main/java/org/java/codewars/RemoveStringSpaces8kyu.java
+++ JavaWars/src/main/java/org/java/codewars/RemoveStringSpaces8kyu.java
@@ -1,7 +1,7 @@
 //https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/java
 package org.java.codewars;
 
-public class RemoveStringSpaces {
+public class RemoveStringSpaces8kyu {
     static void main(String[] args) {
         System.out.println(noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
     }

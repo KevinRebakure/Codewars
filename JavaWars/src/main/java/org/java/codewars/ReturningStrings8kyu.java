88302e5966db8869c82b61f9ead53271bd509698
Rebakure <kevinrebakure@gmail.com>
2025-10-13 11:18:03 +0200

    returning strings 8kyu

---
+++ JavaWars/src/main/java/org/java/codewars/ReturningStrings8kyu.java
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/train/java
+package org.java.codewars;
+
+public class ReturningStrings8kyu {
+    static void main(String[] args) {
+        System.out.println(greet("Kevin Rebakure"));
+    }
+    public static String greet(String name) {
+        return String.format("Hello, %s how are you doing today?", name);
+    };
+}

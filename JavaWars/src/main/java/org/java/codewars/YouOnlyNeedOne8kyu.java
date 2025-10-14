e9e1f97ade46a521bde6d8052d596ee5632c380e
Rebakure <kevinrebakure@gmail.com>
2025-10-14 08:04:45 +0200

    check if a value exists in an array

---
+++ JavaWars/src/main/java/org/java/codewars/YouOnlyNeedOne8kyu.java
@@ -0,0 +1,13 @@
+package org.java.codewars;
+
+import java.util.Arrays;
+
+public class YouOnlyNeedOne8kyu {
+    static void main(String[] args) {
+
+    }
+
+    public static boolean check(Object[] a, Object x) {
+        return Arrays.asList(a).contains(x);
+    }
+}

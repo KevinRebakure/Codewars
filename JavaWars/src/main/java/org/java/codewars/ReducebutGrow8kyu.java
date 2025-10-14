6f1939219f888ca579b59c556fc5e9fc95151195
Rebakure <kevinrebakure@gmail.com>
2025-10-14 07:43:26 +0200

    reduce but grow

--- JavaWars/src/main/java/org/java/codewars/ReducebutGrow8kyu.java
+++ JavaWars/src/main/java/org/java/codewars/ReducebutGrow8kyu.java
@@ -1,4 +1,17 @@
 package org.java.codewars;
 
 public class ReducebutGrow8kyu {
+    static void main(String[] args) {
+
+    }
+
+    public static int grow(int[] x){
+        int result = 1;
+
+        for (int i = 0; i < x.length; i++) {
+            result *= x[i];
+        }
+        return result;
+
+    }
 }

e63e3fc175b860480cf11a4e9c60a717cc782179
Rebakure <kevinrebakure@gmail.com>
2025-10-14 07:35:45 +0200

    smash sentence

---
+++ JavaWars/src/main/java/org/java/codewars/SmashSentence.java
@@ -0,0 +1,12 @@
+package org.java.codewars;
+
+public class SmashSentence {
+    public static String smash(String... words) {
+        return String.join(" ", words).trim();
+    }
+
+    static void main(String[] args) {
+        System.out.println(smash(new String[]{"hello", "world", "this", "is", "great"}));
+    }
+
+}

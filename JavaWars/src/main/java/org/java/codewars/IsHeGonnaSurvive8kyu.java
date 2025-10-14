912a43ad96ce95743789ca7939a87ec0daae1e9b
Rebakure <kevinrebakure@gmail.com>
2025-10-14 07:50:39 +0200

    is he gonna survive 8 kyu

---
+++ JavaWars/src/main/java/org/java/codewars/IsHeGonnaSurvive8kyu.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/java
+package org.java.codewars;
+
+public class IsHeGonnaSurvive8kyu {
+    static void main(String[] args) {
+
+    }
+
+    public static boolean hero(long bullets, long dragons) {
+      return bullets >= dragons * 2;
+    }
+}

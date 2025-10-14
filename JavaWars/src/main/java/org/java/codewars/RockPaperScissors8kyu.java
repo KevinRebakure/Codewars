3cfae4a611726830a86172ba88d2a76b51175d4f
Rebakure <kevinrebakure@gmail.com>
2025-10-14 08:36:05 +0200

    using hashmap

--- JavaWars/src/main/java/org/java/codewars/RockPaperScissors8kyu.java
+++ JavaWars/src/main/java/org/java/codewars/RockPaperScissors8kyu.java
@@ -1,6 +1,9 @@
 //https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/java
 package org.java.codewars;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RockPaperScissors8kyu {
     public static String rps(String p1, String p2) {
         if (p1 == "scissors" && p2 == "paper") return "Player 1 won!";
@@ -13,7 +16,18 @@ public class RockPaperScissors8kyu {
         return "";
     }
 
+    public static String rps2(String p1, String p2) {
+        if (p1.equals(p2)) return "Draw!";
 
+        Map<String, String> game = new HashMap<>();
+        game.put("scissors", "paper");
+        game.put("paper", "rock");
+        game.put("rock", "scissors");
+
+        String result = game.get(p1);
+        if (result.equals(p2)) return "Player 1 won!";
+        else                   return "Player 2 won!";
+    }
 
     static void main(String[] args) {
 

d50f86770363095fec8b3a0a9eb480ac8efc0f7f
Rebakure <kevinrebakure@gmail.com>
2025-10-14 07:54:10 +0200

    Calculate BMI

---
+++ JavaWars/src/main/java/org/java/codewars/CalculateBMI8kyu.java
@@ -0,0 +1,16 @@
+package org.java.codewars;
+
+public class CalculateBMI8kyu {
+    public static String bmi(double weight, double height) {
+        double bmi = weight / (height * height);
+
+        if (bmi <= 18.5) return "Underweight";
+        if (bmi <= 25.0) return "Normal";
+        if (bmi <= 30.0) return "Overweight";
+        return "Obese";
+    }
+
+    static void main(String[] args) {
+
+    }
+}

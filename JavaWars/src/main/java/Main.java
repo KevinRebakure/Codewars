c667b54434398d110b88426a08059fd92d1f60ad
Rebakure <kevinrebakure@gmail.com>
2025-10-15 07:58:33 +0200

    grass hopper 8 kyu

--- JavaWars/src/main/java/Main.java
+++ JavaWars/src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(null == null);
+        Test person = new Test("Kevin", 12);
+    }
+}
+
+class Test {
+    private String name;
+    private int age;
+    public Test(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
---
+++ JavaWars/src/main/java/org/java/codewars/GradeBook8kyu.java
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/55cbd4ba903825f7970000f5/train/java
+package org.java.codewars;
+
+public class GradeBook8kyu {
+    public static char getGrade(int s1, int s2, int s3) {
+        double average = (s1 + s2 + s3) / 3.0;
+
+        if (average < 60) return 'F';
+        if (average < 70) return 'D';
+        if (average < 80) return 'C';
+        if (average < 90) return 'B';
+        return 'A';
+    }
+
+    static void main(String[] args) {
+
+    }
+}

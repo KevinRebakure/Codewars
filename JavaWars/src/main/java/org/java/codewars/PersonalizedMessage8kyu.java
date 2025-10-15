79c4c012220ee657e80c7d3c7228c425a898ceab
Rebakure <kevinrebakure@gmail.com>
2025-10-15 08:46:24 +0200

    personalized message 8 kyu

---
+++ JavaWars/src/main/java/org/java/codewars/PersonalizedMessage8kyu.java
@@ -0,0 +1,11 @@
+package org.java.codewars;
+
+public class PersonalizedMessage8kyu {
+    static String greet(String name, String owner) {
+        return name.equalsIgnoreCase(owner) ? "Hello boss" : "Hello guest";
+    }
+
+    static void main(String[] args) {
+
+    }
+}

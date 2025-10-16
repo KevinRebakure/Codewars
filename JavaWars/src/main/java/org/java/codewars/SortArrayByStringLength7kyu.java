8244fbe2e283af55485ef8ed59021b25e8e65016
Rebakure <kevinrebakure@gmail.com>
2025-10-16 09:54:37 +0200

    sort array by string length 7 kyu

---
+++ JavaWars/src/main/java/org/java/codewars/SortArrayByStringLength7kyu.java
@@ -0,0 +1,13 @@
+package org.java.codewars;
+
+import java.util.Arrays;
+
+public class SortArrayByStringLength7kyu {
+    static void main(String[] args) {
+        String[] names= {"abcde", "abc", "d", "de", "abcd"};
+
+        Arrays.sort(names, (a, b) -> Integer.compare(a.length(), b.length()));
+
+        System.out.println(Arrays.toString(names));
+    }
+}

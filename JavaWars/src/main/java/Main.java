60c43b3c478831e08d5163da94b8c90610603953
Rebakure <kevinrebakure@gmail.com>
2025-10-18 11:47:22 +0200

    text box example

--- JavaWars/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class Main {
-    public static void main(String[] args) {
-        LocalDate date1 = LocalDate.ofYearDay(2017, 366);
-        System.out.println(date1);
-    }
-}
---
+++ JavaWars/src/main/java/com/ultimatejava/exercises/Main.java
@@ -0,0 +1,9 @@
+package com.ultimatejava.exercises;
+
+public class Main {
+    public static void main(String[] args) {
+        var textBox = new TextBox();
+        textBox.setText("Kevin");
+        System.out.println(textBox.text);
+    }
+}

94667a414583ae2c9d86f83623ce6020461fa556
Rebakure <kevinrebakure@gmail.com>
2025-10-18 14:35:24 +0200

    refactor: extract displaying logic

--- JavaWars/src/main/java/com/ultimatejava/mortagagecalculator/Main.java
+++ JavaWars/src/main/java/com/ultimatejava/mortagagecalculator/Main.java
@@ -1,6 +1,4 @@
 package com.ultimatejava.mortagagecalculator;
-import java.text.NumberFormat;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,27 +6,7 @@ public class Main {
         float annualInterest = (float) new ReadInput(1, 30).readNumber("Annual Interest Rate: ");
         byte years = (byte) new ReadInput(1, 30).readNumber("Period (Years): ");
 
-        printMortgage(principal, annualInterest, years);
-        printPaymentSchedule(principal, annualInterest, years);
-    }
-
-    private static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = new Calculator(principal, annualInterest, years).calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println();
-        System.out.println("MORTGAGE");
-        System.out.println("--------");
-        System.out.println("Monthly Payments: " + mortgageFormatted);
-    }
-
-    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("----------------");
-        for (short month = 1; month <= years * Calculator.MONTHS_IN_YEAR; month++) {
-            double balance = new Calculator(principal, annualInterest, years).calculateBalance(month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
+        Display.printMortgage(principal, annualInterest, years);
+        Display.printPaymentSchedule(principal, annualInterest, years);
     }
 }
-

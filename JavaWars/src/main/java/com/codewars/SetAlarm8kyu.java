24d3a3d15f587558ec72dc4c00238f778390bc57
Rebakure <kevinrebakure@gmail.com>
2025-10-20 16:42:06 +0200

    set alarm 8 kyu

---
+++ JavaWars/src/main/java/com/codewars/SetAlarm8kyu.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/java
+package com.codewars;
+
+public class SetAlarm8kyu {
+    static void main(String[] args) {
+
+    }
+
+    public static boolean setAlarm(boolean employed, boolean vacation) {
+        return employed && !vacation;
+    }
+}

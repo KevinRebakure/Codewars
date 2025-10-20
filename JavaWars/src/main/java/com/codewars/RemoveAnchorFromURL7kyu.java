bbab69e70c438371018779fa82bd9c8e1188bf3c
Rebakure <kevinrebakure@gmail.com>
2025-10-20 15:51:19 +0200

    remove url anchor with regex

--- JavaWars/src/main/java/com/codewars/RemoveAnchorFromURL7kyu.java
+++ JavaWars/src/main/java/com/codewars/RemoveAnchorFromURL7kyu.java
@@ -1,9 +1,7 @@
 //https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/java
 package com.codewars;
 
-import java.util.Arrays;
-
-public class RemoveAnchorFromURL {
+public class RemoveAnchorFromURL7kyu {
     static void main(String[] args) {
         System.out.println(removeUrlAnchor("www.codewars.com#about"));
     }
@@ -11,5 +9,9 @@ public class RemoveAnchorFromURL {
     public static String removeUrlAnchor(String url) {
         String[] modified = url.split("#");
         return modified[0];
+    }
+
+    static String removeUrlAnchorWithRegEx(String url) {
+        return url.replaceAll("#.*", "");
     }
 }

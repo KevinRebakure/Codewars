daebe1d872d49aceea98357f00d6d48e00360a29
Rebakure <kevinrebakure@gmail.com>
2025-10-23 22:35:20 +0200

    find greatest number in a file

---
+++ JavaWars/src/main/java/com/hyperskill/exercises/FindGreatestNumberInAFile.java
@@ -0,0 +1,24 @@
+package com.hyperskill.exercises;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class FindGreatestNumberInAFile {
+    static void main(String[] args) {
+        String filePath = String.valueOf(Paths.get("src/main/java/com/hyperskill/exercises/assets/dataset_91007.txt"));
+        File file = new File(filePath);
+
+        try (Scanner scanner = new Scanner(file)){
+            List<Integer> numbers = new ArrayList<>();
+            while (scanner.hasNext()) {
+                numbers.add(scanner.nextInt());
+            }
+            System.out.println(Collections.max(numbers));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+
+    }
+}

8dd493b4f758ce354e4f1ed2d2f41082972e34a1
Rebakure <kevinrebakure@gmail.com>
2025-10-25 18:09:05 +0200

    print elements of a stream

---
+++ JavaWars/src/main/java/com/hyperskill/exercises/PrintElementsOfaStream.java
@@ -0,0 +1,22 @@
+package com.hyperskill.exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class PrintElementsOfaStream {
+    /**
+     * Prints each element from a given stream except first 2 elements.
+     *
+     * @param stream the input stream of integers
+     */
+    public static void printStream(Stream<Integer> stream) {
+        stream.skip(2).forEach(System.out::println);
+    }
+
+    // Don't change the code below
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        printStream(Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt));
+    }
+}
--- JavaWars/src/main/java/com/test/Main.java
+++ JavaWars/src/main/java/com/test/Main.java
@@ -1,20 +1,14 @@
 package com.test;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Paths;
+import java.util.List;
 
-public class Main {
+class Main{
     static void main(String[] args) {
-        String pathName = String.valueOf(Paths.get("src/main/java/com/test/text.txt"));
+        List<String> companies = List.of(
+                "Google", "Amazon", "Samsung",
+                "GOOGLE", "amazon", "Oracle"
+        );
 
-        File file = new File(pathName);
-        try(FileWriter fileWriter = new FileWriter(file, true)) {
-            fileWriter.write("I am the founder of rebakure.com\\n");
-        } catch (IOException e) {
-            System.out.println("File not found");
-        }
-
+        companies.stream().map(String::toUpperCase).distinct().forEach(System.out::println);
     }
 }

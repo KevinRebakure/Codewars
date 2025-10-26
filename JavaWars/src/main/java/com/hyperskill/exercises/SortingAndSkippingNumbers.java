f628de40a57e38db7d5521652d5afef6955fc0b2
Rebakure <kevinrebakure@gmail.com>
2025-10-26 17:47:34 +0200

    sorting and skipping numbers

---
+++ JavaWars/src/main/java/com/hyperskill/exercises/SortingAndSkippingNumbers.java
@@ -0,0 +1,25 @@
+package com.hyperskill.exercises;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class SortingAndSkippingNumbers {
+    public static List<Integer> processNumbers(Collection<Integer> numbers) {
+        return numbers.stream().sorted().filter(x -> x >= 10).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Collection<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(HashSet::new));
+
+        String result = processNumbers(numbers).stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(result);
+    }
+}

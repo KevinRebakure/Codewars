910ab36154cba0353e120aaead79d69533fcc591
Rebakure <kevinrebakure@gmail.com>
2025-10-26 17:04:44 +0200

    sum of divisors

--- JavaWars/src/main/java/com/test/Main.java
+++ JavaWars/src/main/java/com/test/Main.java
@@ -1,14 +1,10 @@
 package com.test;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 class Main{
     static void main(String[] args) {
-        List<String> names = List.of("Patrick Ross", "Kelly Wood", "James Moore", "Janice Coleman", "Mary Carter");
-        long something= names.stream()
-                .filter(name -> name.length() < 12)
-                .filter(name -> name.startsWith("J"))
-                .count();
-        System.out.println(something);
+        long result = Stream.of(1, 2, 3, 4, 5).reduce(1, (x, y) -> x * y);
+        System.out.println(result);
     }
 }

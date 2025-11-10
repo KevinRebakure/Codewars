7e0cbe70d6c5c1f680e15bc5e6c0b37ad54a45cd
Rebakure <kevinrebakure@gmail.com>
2025-11-10 15:58:13 +0200

    using skip and limit

--- JavaWars/src/main/java/com/codewars/SumWithoutHighestAndLowestNumber8kyu.java
+++ JavaWars/src/main/java/com/codewars/SumWithoutHighestAndLowestNumber8kyu.java
@@ -1,5 +1,6 @@
 package com.codewars;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -14,6 +15,12 @@ class KataSum {
         if (numbers == null || numbers.length < 2) return 0;
         List<Integer> sortedNumbers = IntStream.of(numbers).sorted().boxed().toList().subList(1, numbers.length - 1);
         return sortedNumbers.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public static int method2(int[] numbers) {
+        if(numbers == null || numbers.length < 2) return 0;
+        Arrays.sort(numbers);
+        return Arrays.stream(numbers).skip(1).limit(numbers.length-2).sum();
     }
 }
 

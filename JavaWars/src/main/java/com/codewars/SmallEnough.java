9f6ff6f0d1d70cfa631f07a6b026df6fb7238d2f
Rebakure <kevinrebakure@gmail.com>
2025-11-10 15:41:22 +0200

    7 kyu Small enough? - Beginner

---
+++ JavaWars/src/main/java/com/codewars/SmallEnough.java
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/57cc981a58da9e302a000214/train/java
+package com.codewars;
+
+import java.util.Arrays;
+
+public class SmallEnough {
+}
+
+class Kata {
+    public static boolean smallEnough(int[] a, int limit) {
+        return Arrays.stream(a).allMatch( x -> x < limit || x == limit);
+    }
+}

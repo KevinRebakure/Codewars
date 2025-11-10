245f582cb35f240bef0e5c9c5c5cfade32c555e4
Rebakure <kevinrebakure@gmail.com>
2025-11-10 15:56:30 +0200

    8 kyu sum without highest and lowest number

---
+++ JavaWars/src/main/java/com/codewars/SumWithoutHighestAndLowestNumber8kyu.java
@@ -0,0 +1,21 @@
+package com.codewars;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class SumWithoutHighestAndLowestNumber8kyu {
+    static void main(String[] args) {
+        System.out.println(KataSum.sum(new int[] { 6, 2, 1, 8, 10}));
+    }
+}
+
+class KataSum {
+    public static int sum(int[] numbers) {
+        if (numbers == null || numbers.length < 2) return 0;
+        List<Integer> sortedNumbers = IntStream.of(numbers).sorted().boxed().toList().subList(1, numbers.length - 1);
+        return sortedNumbers.stream().mapToInt(Integer::intValue).sum();
+    }
+}
+
+// sort it
+// slice

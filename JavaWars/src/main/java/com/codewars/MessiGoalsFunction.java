efbadd6a2edeefeca10a5c0f60242d63c6b1c764
Rebakure <kevinrebakure@gmail.com>
2025-11-11 17:26:41 +0200

    8 kyu Grasshopper - Messi goals function

---
+++ JavaWars/src/main/java/com/codewars/MessiGoalsFunction.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/java
+package com.codewars;
+
+public class MessiGoalsFunction {
+    static void main(String[] args) {
+
+    }
+
+    static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}

aaef50870eaa5ef334f0796dff0113975cc5172f
Rebakure <kevinrebakure@gmail.com>
2025-11-11 17:50:09 +0200

    8 kyu Parse nice int from char problem

---
+++ JavaWars/src/main/java/com/codewars/ParseNiceIntFromCharProblem8kyu.java
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/java/
+package com.codewars;
+
+import java.util.Arrays;
+
+public class ParseNiceIntFromCharProblem8kyu {
+    static void main(String[] args) {
+        System.out.println(howOld("1 year old"));
+    }
+
+    static int howOld(final String herOld) {
+        return Integer.parseInt(Arrays.stream(herOld.split(" ")).toList().getFirst());
+    }
+}
